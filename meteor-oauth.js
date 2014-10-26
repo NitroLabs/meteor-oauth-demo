269d401dcc4ae0c0b94b1dee8b18c1238eb3bffc
Max Ferguson <maxkferg@gmail.com>
2014-10-26 19:33:03 +0000

    Added facebook-collections package for testing

--- meteor-oauth.js
+++ meteor-oauth.js
@@ -1,7 +1,17 @@
-if (Meteor.isClient) {
+// Set the redirect url for local development
+// Meteor.absoluteUrl.defaultOptions.rootUrl = "http://umkk03403d51.maxkferg.koding.io/";
 
-  Template.hello.loggedIn = function () {
-    return Meteor.user();
-  };
+if (Meteor.isClient){
+    var scopes = ['email','user_photos','user_videos','read_stream']
+    Accounts.ui.config({'requestPermissions':{'facebook':scopes}});
+}
+
+
+if (Meteor.isClient) {
+  Template.helpers({
+    "loggedIn":function () {
+        return Meteor.user();
+    }
+  });
 }
 
